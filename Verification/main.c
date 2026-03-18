0b935b6121f88d5fb7a21af8f3d30ab3eee27a8e
Donatien-24 <donatiendjobo24@gmail.com>
2026-03-18 22:04:12 +0100

    Add files via upload

--- Verification/main.c
+++ Verification/main.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int verification(int tab[], int nbVal, int x)
-{
-    int i;
-    for(i=1; i<=nbVal; i++){
-        if(tab[i]==x){
+int verification_1(int tab[], int nbVal, int x){
+    int cpt;
+
+    for(cpt = 1; cpt <= nbVal; cpt++){
+        if(tab[cpt] == x){
             return 1;
-        }else{
-            return 0;
         }
     }
+    return 0;
 }
+
+int verification_2(int tab[], int nbVal, int x){
+
+    int cpt, reponse;
+
+    for(cpt = 1; cpt <= nbVal; cpt++){
+        if(tab[cpt] == x){
+            reponse = 1;
+        }
+        else{
+            reponse = 0;
+        }
+    }
+    return reponse;
+}
+
+
 int main()
 {
-   int tab[]= {10,8,9,7,4};
-   const int nbVal= sizeof(tab)/sizeof(tab[0]);
-   int resultat;
-   resultat=verification(tab, nbVal, 8);
-   printf("\nResultat = %d",resultat);
+
+    setlocale(LC_CTYPE, "");
+
+    int tab[] = {10,8,9,7,4};
+    const int nbVal = sizeof(tab)/ sizeof(tab[0]);
+    int resultat, resultat_1;
+    resultat = verification_1(tab,nbVal,8);
+    resultat_1 = verification_2(tab,nbVal,8);
+
+    printf("\nRésultat = %d", resultat);
+    printf("\nRésultat = %d", resultat_1);
+    return 0;
 }
