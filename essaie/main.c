ab4a0f54697cba4ce25c06ec4d42cd49ee9ab6b7
Donatien-24 <donatiendjobo24@gmail.com>
2026-03-21 21:56:02 +0100

    Add files via upload

---
+++ essaie/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int essaie_1(int tab[], int nbVal, int x){
+    int cpt = 0;
+
+    while(cpt <= nbVal){
+        if(tab[cpt] == x){
+            return 1;
+        }
+        cpt++;
+    }
+    return 0;
+}
+
+int essaie_2(int tab[], int nbVal, int x){
+
+    int cpt = 0;
+    int reponse = 0;
+
+    while(cpt <= nbVal){
+        if(tab[cpt] == x){
+            reponse = 1;
+        }
+        else{
+            reponse = 0;
+        }
+        cpt++;
+    }
+    return reponse;
+}
+
+
+int main()
+{
+
+    setlocale(LC_CTYPE, "");
+
+    int tab[] = {10,8,9,7,4};
+    const int nbVal = sizeof(tab)/ sizeof(tab[0]);
+    int resultat, resultat_1;
+    resultat = essaie_1(tab,nbVal,8);
+    resultat_1 = essaie_2(tab,nbVal,8);
+
+    printf("\nRésultat = %d", resultat);
+    printf("\nRésultat = %d", resultat_1);
+    return 0;
+}
